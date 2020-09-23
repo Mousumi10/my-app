07aba49a724fa7d113ad4a01612b54ceaf2886b6
Mousumi10 <mousumi.mukherjee1001@gmail.com>
2020-09-23 22:35:40 +0530

    cropped code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,9 +9,9 @@ import com.amazonaws.services.ec2.model.StartInstancesRequest;
 import com.amazonaws.services.ec2.model.StopInstancesRequest;
 
 @SuppressWarnings("unused")
-public class App implements RequestHandler<App.InputObject, String> {
+public class App implements RequestHandler<Object, String> {
 
-    public String handleRequest(InputObject inputObject, Context context) {
+    public String handleRequest(Object inputObject, Context context) {
         AmazonEC2 ec2Client = AmazonEC2ClientBuilder.standard()
                 .withRegion("us-east-1")
                 .build();
@@ -25,43 +25,5 @@ public class App implements RequestHandler<App.InputObject, String> {
         System.out.println("Started the Instnace with ID: "+instanceId);
         return "{\"result\": \"hello lambda java\"}";
     }  
-
-    public static class InputObject {
-        private String key1;
-        private String key2;
-        private String key3;
-
-        public String getKey1() {
-            return key1;
-        }
-
-        public String getKey2() {
-            return key2;
-        }
-
-        public String getKey3() {
-            return key3;
-        }
-
-        public void setKey1(String key1) {
-            this.key1 = key1;
-        }
-
-        public void setKey2(String key2) {
-            this.key2 = key2;
-        }
-
-        public void setKey3(String key3) {
-            this.key3 = key3;
-        }
-
-        @Override
-        public String toString() {
-            return "InputObject{" +
-                    "key1='" + key1 + '\'' +
-                    ", key2='" + key2 + '\'' +
-                    ", key3='" + key3 + '\'' +
-                    '}';
-        }
-    }
 }
+    
